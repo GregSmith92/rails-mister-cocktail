a8df0684d9819e9ce2877222e8588752c20cbce8
Greg Smith <greg_smith1992@hotmail.com>
2019-05-15 12:47:22 +0100

    Added cl_image_tag to my show page to display an image of the cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -49,7 +49,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
 
   end
 
